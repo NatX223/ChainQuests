80be9509f8e40dec7030eda031d7bded17e89bdf
sinachi ajuzie <Ajuziesinachi@gmail.com>
2024-01-24 02:58:56 +0100

    Done: Frontend development setup

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	plugins: ["react-hooks"],
+	rules: {
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+	},
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+	},
+	env: {
+		es6: true,
+	},
+};
